750c90dabdbe404f5acc97f83a828b058613a2d0
ismaelbelmicro <ismael.patrick@belmicro.com.br>
2023-12-07 17:00:25 -0300

    Funções de hash

---
+++ hash/hash.js
@@ -0,0 +1,31 @@
+import { createHash } from "crypto";
+
+function criaHash(senha) {
+  return createHash("sha256").update(senha).digest("hex");
+}
+
+console.log(criaHash("uma String Qualquer"));
+
+class Usuario {
+  constructor(nome, senha) {
+    this.nome = nome;
+    this.hash = criaHash(senha);
+  }
+  autentica(nome, senha) {
+    if (nome === this.nome && this.hash === criaHash(senha)) {
+      console.log("Usuário autenticado com sucesso!");
+      return true;
+    }
+    console.log("Usuário ou senha incorretos");
+    return false;
+  }
+}
+
+const usuario = new Usuario("João Ismael", "minhaSenha");
+console.log(usuario);
+
+// Sucess
+usuario.autentica("João Ismael", "minhaSenha");
+
+// 400
+usuario.autentica("João Ismael", "minhasenha");
