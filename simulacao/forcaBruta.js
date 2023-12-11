012b41f81080679bcd8aed1042a0ff82714a3d40
ismaelbelmicro <ismael.patrick@belmicro.com.br>
2023-12-11 16:42:26 -0300

    Ataque de força bruta para descobrir senhas

---
+++ simulacao/forcaBruta.js
@@ -0,0 +1,28 @@
+import { createHash } from "crypto";
+
+class Usuario {
+  constructor(nome, senha) {
+    this.nome = nome;
+    this.hash = this.criaHash(senha);
+  }
+
+  criaHash(senha) {
+    return createHash("sha256").update(senha).digest("hex");
+  }
+
+  autentica(nome, senha) {
+    if (nome === this.nome && this.hash === this.criaHash(senha)) {
+      console.log("Usuário autenticado com sucesso!");
+      return true;
+    }
+    return false;
+  }
+}
+
+const usuario = new Usuario("Ismael Patrick", "1337");
+
+for (let senhaTeste = 0; senhaTeste < 10000; senhaTeste++) {
+  if (usuario.autentica("Ismael Patrick", senhaTeste.toString())) {
+    console.log(`A senha do usuário é ${senhaTeste}`);
+  }
+}
